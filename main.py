261b02909dae1de2a2d37d9dc0c9572ee4edbf4b
Kingcode <o2real@naver.com>
2024-09-24 15:38:43 +0900

    alert

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "053a8df930e92e35859269608dafbd10"
+
+weather_params = {
+    "lat": 51.507351,
+    "lon": -0.127758,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
